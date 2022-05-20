1387fc607ae44632a7bd87555b384bfeeb2d15a9
Grygory Ksheminsky <maroon785@gmail.com>
2022-05-20 23:05:30 +0300

    working with showInfo state

--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const showMore = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">btn</button>
+        <button className="btn" onClick={showMore}>
+          btn
+        </button>
       </header>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
